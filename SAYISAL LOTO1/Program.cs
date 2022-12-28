c44f0aa28535741badf6d142108a89ec0854c86a
Ahmetncengiz <119484222+Ahmetncengiz@users.noreply.github.com>
2022-12-28 20:58:19 +0300

    Add files via upload

---
+++ SAYISAL LOTO1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] sayilar=new int[6];
+int[] kasaSayilar=new int[6];
+int tahmin = 0;
+
+Console.WriteLine("lütfen 6 sayı giriniz");
+Random rnd=new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    kasaSayilar[i]=rnd.Next(1,60);
+    Console.WriteLine("{0}.  sayıyı giriniz :", (i+1) );
+    sayilar[i]=int.Parse(Console.ReadLine()!);
+}
+foreach(var sayı in  sayilar)
+{
+    foreach(var kasaSayi in  kasaSayilar)
+    {
+       tahmin = sayı==kasaSayi? tahmin++ : tahmin;
+    }
+}
+Console.WriteLine("{0} adet doğru tahminde bulundunuz",tahmin);
+Console.WriteLine("random sayilar: {0}",kasaSayilar.ToString());
+        for (int i = 0; i < kasaSayilar.Length; i++)
+{
+    
+}
+{
+    Console.WriteLine("{0}", kasaSayilar[i]);
+}
